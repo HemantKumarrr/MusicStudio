02fd2bcfbfbb57d4c83002b7684f10231591703c
Hemant Kumar <91686097+HemantK29@users.noreply.github.com>
2023-03-18 14:22:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-const r = document.getElementById("r");
-const t = document.getElementById("t");
-
-function c(){
-    t.value = this.value;
+function sendMail(){
+    let params = {
+        name : document.getElementById("name").value,
+        // message : document.getElementById("phoneNumber").value,
+        message : document.getElementById("message").value,
+    };
+    const serviceID = "service_6o7zb6s";
+    const tempID = "template_qams8fk";
+    
+    emailjs.send(serviceID, tempID, params)
+    .then(
+        res =>{
+            document.getElementById("name").value = "";
+            // document.getElementById("phoneNumber").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("Your message sent successfully");
+    
+        }
+    )
+    .catch((err)=> console.log(err));
 }
-
-r.oninput = c;
-
